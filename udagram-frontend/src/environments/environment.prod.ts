4f98429d27d59a9fe0328e65524354289544d484
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-06-04 22:15:19 +0100

    new mod

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2f12c57385884b2ba3084e63c1115cb-1885449496.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
